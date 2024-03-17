7af4b1b94a408d7632724e62630a408d5018edd4
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-03-17 13:14:19 +0100

    Add Backroud mush and ladning page redirect to client page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import cn from "classnames";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import cn from "classnames";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn(inter.className, "h-full")}>{children}</body>
+      <body className={cn(inter.className, "h-full background-primary")}>
+        {/* Première lueur en bas à droite */}
+        <div className="glow-container bottom-right">
+          <svg
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+            className="glow-svg"
+          >
+            <path
+              fill="#00FF75"
+              d="M46.4,-79.9C60.8,-72.2,73.4,-60.9,79.9,-47C86.5,-33.1,87,-16.5,86.7,-0.2C86.3,16.1,85,32.2,78.6,46.4C72.2,60.5,60.6,72.7,46.6,79.3C32.7,85.9,16.3,87,0,86.9C-16.3,86.9,-32.6,85.8,-46.4,79.1C-60.1,72.4,-71.4,60,-78.5,45.9C-85.6,31.8,-88.6,15.9,-88.5,0C-88.5,-15.8,-85.3,-31.6,-78.1,-45.6C-70.9,-59.6,-59.7,-71.8,-46,-79.9C-32.3,-88,-16.2,-92,0,-92C16.1,-91.9,32.1,-87.7,46.4,-79.9Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+
+        {/* Deuxième lueur au milieu à gauche */}
+        <div className="glow-container middle-left">
+          <svg
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+            className="glow-svg"
+          >
+            <path
+              fill="#00FFD1"
+              d="M46.4,-79.9C60.8,-72.2,73.4,-60.9,79.9,-47C86.5,-33.1,87,-16.5,86.7,-0.2C86.3,16.1,85,32.2,78.6,46.4C72.2,60.5,60.6,72.7,46.6,79.3C32.7,85.9,16.3,87,0,86.9C-16.3,86.9,-32.6,85.8,-46.4,79.1C-60.1,72.4,-71.4,60,-78.5,45.9C-85.6,31.8,-88.6,15.9,-88.5,0C-88.5,-15.8,-85.3,-31.6,-78.1,-45.6C-70.9,-59.6,-59.7,-71.8,-46,-79.9C-32.3,-88,-16.2,-92,0,-92C16.1,-91.9,32.1,-87.7,46.4,-79.9Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
+
+        {/* Contenu avec style CSS pour le positionner correctement */}
+        <div className="content">{children}</div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hello World</p>
-      <Button>Click me</Button>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-col gap-2 items-center">
+        <p className="text-8xl font-bold text-white">Stop Waste Time</p>
+        <p className="text-8xl font-bold text-white">
+          With <span className="color-text-primary">Taskify</span>
+        </p>
+        <Button
+          size={"lg"}
+          color="secondary"
+          onClick={() => {
+            window.location.href = "/client";
+          }}
+          className="mt-[3%] button-primary"
+        >
+          Get Started
+        </Button>
+      </div>
     </div>
   );
 }
